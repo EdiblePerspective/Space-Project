4c32982fe2e0a02fcc9949abf8296ee7f4ebf4de
EdiblePerspective <ryancox430@gmail.com>
2023-12-01 22:45:38 +0000

    Add stuff

---
+++ app.js
@@ -0,0 +1,22 @@
+const navHeader = document.getElementById('navHeader');
+
+navHeader.addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+    console.dir(event.target.id);
+    console.log("Confirmation Recieved... Nuclear Device Launched at Target Location")
+})
+function trash(){
+let A=prompt("Are you sure you want to delete 'C:\Windows\System32'?")
+  if (A!=null){
+    alert("Positive confirmation recieved, deletion in progress...")
+  }
+}
+const song = document.getElementById("song");
+function playTheSongOfMyPeople(){
+  audioPlay.play();
+}
+song.addEventListener("click", playTheSongOfMyPeople);
+
